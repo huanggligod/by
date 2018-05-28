402e292ddf55c907ac407d25eae6509376c6d7b4
wang <1014205876@qq.com>
2018-05-28 19:47:13 +0800

    wang

---
+++ src/pages/index/wang.vue
@@ -0,0 +1,21 @@
+<template>
+<div>wang</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+		}
+	},
+	components: {
+	},
+	methods: {
+	},
+	created () {
+	}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
