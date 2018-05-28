952bbf897d5aed5bcb80a4e1032d46345a21f928
huanggang <516812614@qq.com>
2018-05-28 19:14:45 +0800

    by

---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="index">
+		<!-- <swiper></swiper> -->
+		<img class="img" src="/static/image/swiper.png" alt="banner">
+
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+		}
+	},
+	components: {
+	},
+	methods: {
+	},
+	created () {
+	}
+}
+</script>
+
+<style lang='less' scoped>
+	.index {
+		background: #f9f5f6;
+	}
+	.img {
+		display: block;
+		width: 100%;
+		height: 300rpx;
+	}
+</style>
